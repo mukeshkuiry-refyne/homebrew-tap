e1e43fc2d7886cd09380de2bea20915a03e154d2
Mukesh kuiry <mukesh.kuiry@refyne.co.in>
2026-05-23 19:31:08 +0530

    v1.0.4: fix segfault, all QA passed

--- Formula/runway-meeting.rb
+++ Formula/runway-meeting.rb
@@ -1,8 +1,8 @@
 class RunwayMeeting < Formula
   desc "Aviation-themed macOS menu bar app for Google Calendar"
   homepage "https://github.com/mukeshkuiry/runway"
-  url "https://github.com/mukeshkuiry/runway/releases/download/v1.0.3/runway-meeting-v1.0.2-darwin-arm64.tar.gz"
-  sha256 "d787670f1bfa327b235cefe8a319da070c88b8680e085428d4ff0425de08d9d7"
+  url "https://github.com/mukeshkuiry/runway/releases/download/v1.0.4/runway-meeting-darwin-arm64.tar.gz"
+  sha256 "f3fc38789f119bbab394013ce20b0c0841f6d9393b9ace8fee3130ae49ee6fd5"
   license "MIT"
 
   depends_on macos: :ventura
