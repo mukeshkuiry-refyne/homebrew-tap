b73eccce27d2940ee505d3960095497b8411f620
Mukesh kuiry <mukesh.kuiry@refyne.co.in>
2026-05-23 19:10:33 +0530

    Fix: resolve binary path for posix_spawn

--- Formula/runway-meeting.rb
+++ Formula/runway-meeting.rb
@@ -2,7 +2,7 @@ class RunwayMeeting < Formula
   desc "Aviation-themed macOS menu bar app for Google Calendar"
   homepage "https://github.com/mukeshkuiry/runway"
   url "https://github.com/mukeshkuiry/runway/releases/download/v1.0.0/runway-meeting-v1.0.0-darwin-arm64.tar.gz"
-  sha256 "87ba3f8ed2585ea74270ee993da6eefecd1b8bd055c893bb912292e591fc6f40"
+  sha256 "1c090fc60c79a79c46d28dbd3a29c25aa18490a57cb4c4fad184373ed27a899c"
   license "MIT"
 
   depends_on macos: :ventura
