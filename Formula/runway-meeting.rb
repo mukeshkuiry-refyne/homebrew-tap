1dfb8e7b7a9331689adbafef0876a84c23ee560f
Mukesh kuiry <mukesh.kuiry@refyne.co.in>
2026-05-23 19:26:14 +0530

    v1.0.3: fix duplicate start detection

--- Formula/runway-meeting.rb
+++ Formula/runway-meeting.rb
@@ -1,8 +1,8 @@
 class RunwayMeeting < Formula
   desc "Aviation-themed macOS menu bar app for Google Calendar"
   homepage "https://github.com/mukeshkuiry/runway"
-  url "https://github.com/mukeshkuiry/runway/releases/download/v1.0.2/runway-meeting-v1.0.1-darwin-arm64.tar.gz"
-  sha256 "2fb9b14d78e6b3a0b6357eb05de8c03200ea5901e0186a48e5dd40ada7566bf3"
+  url "https://github.com/mukeshkuiry/runway/releases/download/v1.0.3/runway-meeting-v1.0.2-darwin-arm64.tar.gz"
+  sha256 "d787670f1bfa327b235cefe8a319da070c88b8680e085428d4ff0425de08d9d7"
   license "MIT"
 
   depends_on macos: :ventura
